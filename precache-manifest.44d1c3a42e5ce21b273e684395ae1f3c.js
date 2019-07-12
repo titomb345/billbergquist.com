f2a931cccde0e03f8d11918658d0f608b5da6d62
Bill Bergquist <titomb345@gmail.com>
2019-07-12 13:21:48 -0700

    Updates

---
+++ precache-manifest.44d1c3a42e5ce21b273e684395ae1f3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c05eeb84c3572e7092e48c66d513469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47fb6b56e11a91a254f0",
+    "url": "/static/css/0.53fdc6f9.chunk.css"
+  },
+  {
+    "revision": "acfff4d099ef662ac9a3",
+    "url": "/static/css/3.35d3eea9.chunk.css"
+  },
+  {
+    "revision": "47fb6b56e11a91a254f0",
+    "url": "/static/js/0.f7be5189.chunk.js"
+  },
+  {
+    "revision": "acfff4d099ef662ac9a3",
+    "url": "/static/js/3.589bbaa5.chunk.js"
+  },
+  {
+    "revision": "04c1560fb3b452fc349f",
+    "url": "/static/js/4.b642afae.chunk.js"
+  },
+  {
+    "revision": "a16a5fe2c1614b503d17",
+    "url": "/static/js/main.280f95fa.chunk.js"
+  },
+  {
+    "revision": "5a8d7a3ee6268badf82e",
+    "url": "/static/js/runtime~main.0ac81bb9.js"
+  },
+  {
+    "revision": "aad264a59153d1a4bc52cf034dee828d",
+    "url": "/static/media/no-avatar.aad264a5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.44d1c3a42e5ce21b273e684395ae1f3c.js"
 );
 
 self.addEventListener('message', (event) => {
